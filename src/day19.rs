ae4567c30b2ca07a04b6d3e3be45de6578acbb1a
Sergio Giraldo <sergio@giraldo.com.br>
2024-01-25 18:38:44 +0100

    feat!: day 20 and 21 completed ::by sergio giraldo @ 20240125T1838CET, gpg signed

---
+++ src/day19.rs
@@ -0,0 +1,96 @@
+use crate::utils;
+extern crate regex;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+extern crate itertools;
+use itertools::Itertools;
+
+fn parse_replacements(s: &str) -> Vec<(String, String)> {
+    s.lines()
+        .map(|l| {
+            let words: Vec<_> = l.split_whitespace().collect();
+            (words[0].to_owned(), words[2].to_owned())
+        })
+        .collect()
+}
+
+fn apply_replacements(input: &str, replacements: &[(String, String)]) -> usize {
+    let input = input.to_owned();
+    let mut molecules: HashSet<String> = HashSet::new();
+
+    for (replacement_s, replacement_r) in replacements {
+        let local_input = input.clone();
+
+        let regex = Regex::new(&format!("({})", replacement_s)).unwrap();
+
+        for m in regex.find_iter(&local_input) {
+            let pre_input = local_input[0..m.start()].to_string();
+            let post_input = local_input[m.end()..].to_string();
+
+            let molecule = format!("{}{}{}", pre_input, replacement_r, post_input);
+            molecules.insert(molecule);
+        }
+    }
+
+    molecules.len()
+}
+
+fn synthesize_molecule(input: &str, replacements: &[(String, String)], target: &str) -> i32 {
+    let replacements: HashMap<String, String> = replacements
+        .iter()
+        .map(|r| (r.1.chars().rev().collect(), r.0.chars().rev().collect()))
+        .collect();
+
+    let mut count = 0;
+    let mut actual_target = target.chars().rev().join("");
+    let regex = Regex::new(&replacements.keys().join("|")).unwrap();
+
+    while actual_target != input {
+        actual_target = regex
+            .replace(&actual_target, |captures: &regex::Captures| {
+                replacements.get(&captures[0]).unwrap()
+            })
+            .to_string();
+
+        count += 1;
+    }
+
+    count
+}
+
+#[aoc(day19, part1)]
+pub fn run(input: &str) -> String {
+    let split_regex = Regex::new(r#"\n\n"#).unwrap();
+    let parts: Vec<_> = split_regex.split(&input).collect();
+
+    let replacements = parse_replacements(parts[0]);
+
+    let res = apply_replacements(parts[1], &replacements).to_string();
+
+    utils::solve(2015, 19, "1", &res);
+
+    res
+}
+
+#[aoc(day19, part2)]
+pub fn run_pt2(input: &str) -> String {
+    let split_regex = Regex::new(r#"\n\n"#).unwrap();
+    let parts: Vec<_> = split_regex.split(&input).collect();
+
+    let replacements = parse_replacements(parts[0]);
+    
+    let res = synthesize_molecule(&"e", &replacements, parts[1]).to_string();
+
+    utils::solve(2015, 19, "2", &res);
+
+    res
+}
+
+#[test]
+fn test_run() {
+    const TEST_REPLACEMENTS: &str = r#"H => HO
+    H => OH
+    O => HH"#;
+    let replacements = parse_replacements(&TEST_REPLACEMENTS);
+    assert_eq!(apply_replacements(&"HOH", &replacements), 4);
+}
---
+++ src/day20.rs
@@ -0,0 +1,87 @@
+use std::iter::FromIterator;
+use crate::utils;
+
+#[aoc(day20, part1)]
+pub fn run(input: &str) -> String {
+    let mut res:String = "".to_string();
+
+    let input_int: usize = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return "Invalid input".to_string(),
+    };
+
+    for house_nr in 1..input_int {
+        if presents_at1(house_nr.try_into().unwrap()) >= input_int.try_into().unwrap() {
+            res = house_nr.to_string();
+            break
+        }
+    }
+
+    utils::solve(2015, 20, "1", &res);
+
+    res
+}
+
+#[aoc(day20, part2)]
+pub fn run_pt2(input: &str) -> String {
+    let mut res:String = "".to_string();
+
+    let input_int: usize = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return "Invalid input".to_string(),
+    };
+
+    for house_nr in 1..input_int {
+        if presents_at2(house_nr.try_into().unwrap()) >= input_int.try_into().unwrap() {
+            res = house_nr.to_string();
+            break
+        }
+    }
+
+    utils::solve(2015, 20, "2", &res);
+
+    res
+}
+
+fn presents_at1(house_nr: u64) -> u64 {
+    divisors(house_nr)
+        .into_iter()
+        .sum::<u64>() * 10
+}
+
+fn presents_at2(house_nr: u64) -> u64 {
+    divisors(house_nr)
+        .into_iter()
+        .filter(|d| house_nr / d <= 50)
+        .sum::<u64>()
+        * 11
+}
+
+fn divisors(n: u64) -> Vec<u64> {
+    let mut small_divisors: Vec<u64> = Vec::from_iter(1..((n as f64).sqrt() as u64 + 1))
+        .into_iter()
+        .filter(|i| n % *i == 0)
+        .collect();
+
+    let mut large_divisors: Vec<u64> = small_divisors
+        .iter()
+        .filter(|d| n != **d * **d)
+        .map(|d| n / d)
+        .collect();
+
+    small_divisors.append(&mut large_divisors);
+    small_divisors
+}
+
+#[test]
+fn test_get_house1_presents() {
+    assert_eq!(presents_at1(1), 10);
+    assert_eq!(presents_at1(2), 30);
+    assert_eq!(presents_at1(3), 40);
+    assert_eq!(presents_at1(4), 70);
+    assert_eq!(presents_at1(5), 60);
+    assert_eq!(presents_at1(6), 120);
+    assert_eq!(presents_at1(7), 80);
+    assert_eq!(presents_at1(8), 150);
+    assert_eq!(presents_at1(9), 130);
+}
